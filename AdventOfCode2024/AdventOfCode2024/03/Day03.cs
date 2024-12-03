0823e3f055a5ac029f0ff836dd78446ab0af5b64
Patrycja Bruzdzinska <patrycja.bruzdzinska@gmail.com>
2024-12-03 18:39:44 +0100

    add day 03 first part

---
+++ AdventOfCode2024/AdventOfCode2024/03/Day03.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024._03
+{
+    public class Day03 : IDay
+    {
+        public string GetFirstAnswer()
+        {
+            var lines = InputReader.ReadInput("03");
+
+            return GetSumOfProducts(lines).ToString();
+        }
+
+        public string GetSecondAnswer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetSumOfProducts(IEnumerable<string> lines)
+        {
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var sumOfMultiplicationsInLine = GetSumOfProductsFromLine(line);
+                sum += sumOfMultiplicationsInLine;
+            }
+            return sum;
+        }
+
+        private int GetSumOfProductsFromLine(string line)
+        {
+            var sum = 0;
+
+            var pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                var x = match.Groups[1].Value;
+                var y = match.Groups[2].Value;
+
+                var product = int.Parse(x) * int.Parse(y);
+                sum += product;
+            }
+
+            return sum;
+        }
+    }
+}
--- AdventOfCode2024/AdventOfCode2024/Program.cs
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2024._01;
 using AdventOfCode2024._02;
+using AdventOfCode2024._03;
 
 Console.WriteLine("Hello, Advent!");
 
@@ -8,5 +9,9 @@ var day01 = new Day01();
 //Console.WriteLine($"Answer for day01: {day01.GetSecondAnswer()}");
 
 var day02 = new Day02();
-//Console.WriteLine($"Answer for day01: {day02.GetFirstAnswer()}");
-Console.WriteLine($"Answer for day01: {day02.GetSecondAnswer()}");
+//Console.WriteLine($"Answer for day02: {day02.GetFirstAnswer()}");
+//Console.WriteLine($"Answer for day02: {day02.GetSecondAnswer()}");
+
+var day03 = new Day03();
+Console.WriteLine($"Answer for day03: {day03.GetFirstAnswer()}");
+//Console.WriteLine($"Answer for day03: {day03.GetSecondAnswer()}");
