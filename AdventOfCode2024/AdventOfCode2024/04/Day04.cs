4e2495eaf89448f204e41edfd3486afa751bc635
Patrycja Bruzdzinska <patrycja.bruzdzinska@gmail.com>
2024-12-04 18:09:25 +0100

    add empty day

---
+++ AdventOfCode2024/AdventOfCode2024/04/Day04.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024._04
+{
+    internal class Day04
+    {
+    }
+}
