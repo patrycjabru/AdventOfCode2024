177534f7ddc199c5225d8073a4d1919491f84f6a
Patrycja Bruzdzinska <patrycja.bruzdzinska@gmail.com>
2024-12-09 21:22:04 +0100

    add day 09 part 1

---
+++ AdventOfCode2024/AdventOfCode2024/09/Day09.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024._09
+{
+    public class Day09 : IDay
+    {
+        public string GetFirstAnswer()
+        {
+            var input = InputReader.ReadInput("09").First();
+
+            return CalculateChecksum(input).ToString();
+        }
+        public string GetSecondAnswer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private long CalculateChecksum(string input)
+        {
+            long checksum = 0;
+            var isFreeSpace = false;
+            var blockCounter = 0;
+            var fileId = 0;
+            var fileIdFromEnd = input.Length / 2 + 1;
+            var fileIndexFromEnd = input.Length - 1;
+            var howManyLeftWithThisId = 0;
+            var iterationsToFinish = int.MaxValue;
+            var flagToFinish = false;
+            var processedFiles = new Dictionary<int, int>(); //key - file id, value - number of blocks processed
+
+            for (int indexFromBegining = 0; indexFromBegining < input.Length; indexFromBegining++)
+            {
+                
+                if (fileId >= fileIdFromEnd)
+                {
+                    if (iterationsToFinish == 0)
+                    {
+                        break;
+                    }
+                    if (!flagToFinish)
+                    {
+                        var size = int.Parse(input[indexFromBegining + 1].ToString());
+                        flagToFinish = true;
+                        iterationsToFinish = size - processedFiles[fileIdFromEnd];
+                    }
+                }
+                    
+                if (!isFreeSpace)
+                {
+                    var fileSize = int.Parse(input[indexFromBegining].ToString());
+                    for (int fileBlockIndex = 0; fileBlockIndex < fileSize; fileBlockIndex++)
+                    {
+                        if (flagToFinish)
+                        {
+                            iterationsToFinish--;
+                        }
+                        checksum += blockCounter * fileId;
+                        if (iterationsToFinish == 0)
+                            break;
+                        UpdateDictionary(fileId, processedFiles);
+                        blockCounter++;
+                    }
+                    fileId++;
+                }
+                else
+                {
+                    var freeSpaceSize = int.Parse(input[indexFromBegining].ToString());
+                    for (int freeBlockIndex = 0; freeBlockIndex < freeSpaceSize; freeBlockIndex++)
+                    {
+                        if (howManyLeftWithThisId == 0)
+                        {
+                            var fileSizeFromEnd = int.Parse(input[fileIndexFromEnd].ToString());
+                            howManyLeftWithThisId = fileSizeFromEnd;
+                            fileIdFromEnd--;
+                            fileIndexFromEnd -= 2;
+                        }
+
+                        if(flagToFinish)
+                        {
+                            iterationsToFinish--;
+                        }
+                        checksum += blockCounter * fileIdFromEnd;
+                        if (iterationsToFinish == 0)
+                            break;
+                        UpdateDictionary(fileIdFromEnd, processedFiles);
+                        blockCounter++;
+                        howManyLeftWithThisId--;
+                    }
+                }
+                isFreeSpace = !isFreeSpace;
+            }
+
+            return checksum;
+        }
+
+        private void UpdateDictionary(int fileId, Dictionary<int, int> processedFiles)
+        {
+            if (processedFiles.ContainsKey(fileId))
+            {
+                processedFiles[fileId]++;
+            }
+            else
+            {
+                processedFiles.Add(fileId, 1);
+            }
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/09/Day09_2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024._09
+{
+    public class Day09_2 : IDay
+    {
+        public string GetFirstAnswer()
+        {
+            var input = InputReader.ReadInput("09").First();
+
+            var processedInput = ProcessInput(input).ToArray();
+            Compress(processedInput);
+            return CalculateChecksum(processedInput).ToString();
+        }
+
+        public string GetSecondAnswer()
+        {
+            throw new NotImplementedException();
+        }
+        private void Compress(int?[] processedInput)
+        {
+            int i = 0;
+            int j = processedInput.Length - 1;
+
+            while (i < j)
+            {
+                if (processedInput[i] == null)
+                {
+                    if (processedInput[j] != null)
+                    {
+                        processedInput[i] = processedInput[j];
+                        processedInput[j] = null;
+                        i++;
+                        j--;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        private long CalculateChecksum(int?[] input)
+        {
+            long checksum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int? inputItem = input[i];
+                if (!inputItem.HasValue)
+                    break;
+                checksum += i * inputItem.Value;
+            }
+            return checksum;
+        }
+
+        private List<int?> ProcessInput(string input)
+        {
+            var processedInput = new List<int?>();
+            var isFile = true;
+            var fileId = 0;
+            foreach (var i in input)
+            {
+                var number = int.Parse(i.ToString());
+                if (isFile)
+                {
+                    for(int n=0; n<number; n++)
+                    {
+                        processedInput.Add(fileId);
+                    }
+                    fileId++;
+                    isFile = !isFile;
+                    
+                }
+                else
+                {
+                    for (int n = 0; n < number; n++)
+                    {
+                        processedInput.Add(null);
+                    }
+                    isFile = !isFile;
+                }
+
+            }
+
+            return processedInput;
+        }
+    }
+}
