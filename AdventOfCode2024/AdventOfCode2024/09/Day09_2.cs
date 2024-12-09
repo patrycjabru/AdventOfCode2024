6ec5bef32304deffe0a0eeb7e5f997623c484fae
Patrycja Bruzdzinska <patrycja.bruzdzinska@gmail.com>
2024-12-09 22:20:10 +0100

    Finished second part

--- AdventOfCode2024/AdventOfCode2024/09/Day09_2.cs
+++ AdventOfCode2024/AdventOfCode2024/09/Day09_2.cs
@@ -19,8 +19,97 @@ namespace AdventOfCode2024._09
 
         public string GetSecondAnswer()
         {
-            throw new NotImplementedException();
+            var input = InputReader.ReadInput("09").First();
+
+            var processedInput = ProcessToArray(input);
+            CompressAndKeepWholeFiles(processedInput);
+            var transformed = TransformToStringOfBlocks(processedInput);
+            return CalculateChecksum(transformed, false).ToString();
         }
+
+        private int?[] TransformToStringOfBlocks(Section[] processedInput)
+        {
+            var output = new List<int?>();
+            foreach (var section in processedInput)
+            {
+                for (var f = 0; f < section.FileSizes.Count; f++)
+                {
+                    for (var i = 0; i < section.FileSizes[f]; i++)
+                    {
+                        output.Add(section.FileIds[f]);
+                    }
+                }
+                var freeSize = section.GetFreeSpace();
+                for (var i = 0; i < freeSize; i++)
+                {
+                    output.Add(null);
+                }
+            }
+            return output.ToArray();
+        }
+
+        private Section[] ProcessToArray(string input)
+        {
+            var output = new List<Section>();
+            var isFile = true;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var size = int.Parse(input[i].ToString());
+                if (isFile)
+                {
+                    output.Add(new Section()
+                    {
+                        FileIds = new List<int>()
+                        {
+                            i/2
+                        },
+                        Size = size,
+                        FileSizes = new List<int>()
+                        {
+                            size
+                        },
+                        PositionId = i
+                    });
+                }
+                else
+                {
+                    output.Add(new Section()
+                    {
+                        Size = size,
+                        PositionId = i
+                    });
+                }
+                isFile = !isFile;
+            };
+
+            return output.ToArray();
+        }
+
+        private void CompressAndKeepWholeFiles(Section[] input)
+        {
+            for (var i = input.Length - 1; i > 0; i--)
+            {
+                var section = input[i];
+                if (section.FileIds.Count == 0)
+                {
+                    continue;
+                }
+
+                var sizeToFind = input[i].FileSizes[0];
+                var freeSlot = input.FirstOrDefault(x => x.GetFreeSpace() >= sizeToFind && x.PositionId < section.PositionId);
+
+                if (freeSlot == null)
+                    continue;
+
+                freeSlot.FileSizes.Add(section.FileSizes[0]);
+                freeSlot.FileIds.Add(section.FileIds[0]);
+              
+
+                section.FileSizes = new List<int>();
+                section.FileIds = new List<int>();
+            }
+        }
+
         private void Compress(int?[] processedInput)
         {
             int i = 0;
@@ -49,14 +138,23 @@ namespace AdventOfCode2024._09
             }
         }
 
-        private long CalculateChecksum(int?[] input)
+        private long CalculateChecksum(int?[] input, bool breakOnNull = true)
         {
             long checksum = 0;
             for (int i = 0; i < input.Length; i++)
             {
                 int? inputItem = input[i];
                 if (!inputItem.HasValue)
-                    break;
+                {
+                    if (breakOnNull)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
                 checksum += i * inputItem.Value;
             }
             return checksum;
---
+++ AdventOfCode2024/AdventOfCode2024/09/Section.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024._09
+{
+    public class Section
+    {
+        public List<int> FileIds { get; set; } = new List<int>();
+        public int Size { get; set; }
+        public List<int> FileSizes { get; set; } = new List<int>();
+
+        public int PositionId { get; set; }
+
+        public int GetFreeSpace()
+        {
+            var filesSizes = 0;
+            foreach (var size in FileSizes)
+            {
+                filesSizes += size;
+            }
+
+            return Size - filesSizes;
+        }
+    }
+}
