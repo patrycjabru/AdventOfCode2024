82efe25ce8839c474464450a1fdf8a40fd8174dc
Patrycja Bruzdzinska <patrycja.bruzdzinska@gmail.com>
2024-12-22 11:34:01 +0100

    Add day 16 part 1

--- AdventOfCode2024/AdventOfCode2024/16/Day16.cs
+++ AdventOfCode2024/AdventOfCode2024/16/Day16.cs
@@ -9,18 +9,18 @@ namespace AdventOfCode2024._16
 {
     public class Day16 : IDay
     {
-        int BestPathScore = int.MaxValue;
-        List<int> bestPathPointsHistory;
-        List<Direction> BestPath;
+        private static readonly int[] dx = { -1, 1, 0, 0 };  // row deltas for up/down
+        private static readonly int[] dy = { 0, 0, -1, 1 };  // col deltas for left/right
         public string GetFirstAnswer()
         {
             var input = InputReader.ReadInput("16");
             var inputAsArray = InputReader.ConvertInputToTwodimmensionalArray(input);
 
-            var start = FindStart(inputAsArray);
-            FindPath(inputAsArray, start, inputAsArray.GetLength(0), inputAsArray.GetLength(1));
+            var (start, end) = FindStartAndEnd(inputAsArray);
 
-            return "";
+            var result = Dijkstra(inputAsArray, start.column, start.row, end.column, end.row);
+
+            return result.ToString();
         }
 
         public string GetSecondAnswer()
@@ -28,104 +28,76 @@ namespace AdventOfCode2024._16
             throw new NotImplementedException();
         }
 
-        private Point? FindStart(char[,] input)
+        private (Point?, Point?) FindStartAndEnd(char[,] input)
         {
+            Point? start = null;
+            Point? end = null;
             for (var i = 0; i < input.GetLength(0); i++)
             {
                 for (var j = 0; j < input.GetLength(1); j++)
                 {
-                    if (input[i,j] == 'S')
-                        return new Point(i, j);
+                    if (input[i, j] == 'S')
+                        start = new Point(i, j);
+                    else if (input[i, j] == 'E')
+                        end = new Point(i, j);
                 }
             }
 
-            return null;
+            return (start, end);
         }
 
-        public void FindPath(char[,] input, Point start, int length, int width)
+        public static int Dijkstra(char[,] maze, int startX, int startY, int endX, int endY)
         {
-            var paths = new List<List<Direction>>();
+            int rows = maze.GetLength(0);
+            int cols = maze.GetLength(1);
+            int[,] distances = InitializeDistances(startX, startY, rows, cols);
 
-            CheckPath(input, new Point(start.row - 1, start.column), length, width, Direction.Up, new List<Direction>(), new HashSet<Point>(), 1000);
-            CheckPath(input, new Point(start.row + 1, start.column), length, width, Direction.Down, new List<Direction>(), new HashSet<Point>(), 1000);
-            CheckPath(input, new Point(start.row, start.column - 1), length, width, Direction.Left, new List<Direction>(), new HashSet<Point>(), 1000);
-            CheckPath(input, new Point(start.row, start.column + 1), length, width, Direction.Right, new List<Direction>(), new HashSet<Point>(), 0);
-        }
+            var pq = new PriorityQueue<(int x, int y, Direction prevDir), int>();
+            pq.Enqueue((startX, startY, Direction.Up), 0);
 
-        public bool CheckPath(char[,] input, Point currentPosition, int length, int width, Direction lastMove, List<Direction> moves, HashSet<Point> positions, int points)
-        {
-            if (points > BestPathScore)
-                return false;
 
-            if (currentPosition.row < 0 || currentPosition.column < 0 || currentPosition.row == length || currentPosition.column == width)
-                return false;
-            
-            if (input[currentPosition.row, currentPosition.column] == '#') 
-                return false;
-
-            var checkIfNooLoop = positions.Add(currentPosition);
-            if (!checkIfNooLoop)
-                return false;
-
-            moves.Add(lastMove);
-
-            if (input[currentPosition.row, currentPosition.column] == 'E')
+            while (pq.Count > 0)
             {
-                foreach (var move in moves)
+                var (currentX, currentY, prevDir) = pq.Dequeue();
+
+                if (currentX == endX && currentY == endY)
                 {
-                    Console.Write(move);
+                    return distances[endX, endY];
                 }
-                Console.WriteLine();
-                BestPathScore = points + 1;
-                BestPath = moves;
-                return true;
-            }
 
-            if (lastMove != Direction.Down)
-            {
-                var newPoints = points;
-                if (lastMove == Direction.Up)
-                    newPoints += 1;
-                else
-                    newPoints += 1001;
-                var upList = new List<Direction>(moves);
-                var upPositions = new HashSet<Point>(positions);
-                CheckPath(input, new Point(currentPosition.row - 1, currentPosition.column), length, width, Direction.Up, upList, upPositions, newPoints);
+                for (int i = 0; i < 4; i++)
+                {
+                    int newX = currentX + dx[i];
+                    int newY = currentY + dy[i];
+
+                    if (newX >= 0 && newX < rows && newY >= 0 && newY < cols && maze[newX, newY] != '#')
+                    {
+                        int directionCost = (prevDir == (Direction)i) ? 1 : 1001;
+                        int newDist = distances[currentX, currentY] + directionCost;
+
+                        if (newDist < distances[newX, newY])
+                        {
+                            distances[newX, newY] = newDist;
+                            pq.Enqueue((newX, newY, (Direction)i), newDist);
+                        }
+                    }
+                }
             }
-            if (lastMove != Direction.Left)
+            return 0;
+        }
+
+        private static int[,] InitializeDistances(int startX, int startY, int rows, int cols)
+        {
+            int[,] distances = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
             {
-                var newPoints = points;
-                if (lastMove == Direction.Right)
-                    newPoints += 1;
-                else
-                    newPoints += 1001;
-                var rightList = new List<Direction>(moves);
-                var rightPositions = new HashSet<Point>(positions);
-                CheckPath(input, new Point(currentPosition.row, currentPosition.column + 1), length, width, Direction.Right, rightList, rightPositions, newPoints);
+                for (int j = 0; j < cols; j++)
+                {
+                    distances[i, j] = int.MaxValue;
+                }
             }
-            if (lastMove != Direction.Up)
-            {
-                var newPoints = points;
-                if (lastMove == Direction.Down)
-                    newPoints += 1;
-                else
-                    newPoints += 1001;
-                var downList = new List<Direction>(moves);
-                var downPositions = new HashSet<Point>(positions);
-                CheckPath(input, new Point(currentPosition.row + 1, currentPosition.column), length, width, Direction.Down, downList, downPositions, newPoints);
-            }
-            if (lastMove != Direction.Right)
-            {
-                var newPoints = points;
-                if (lastMove == Direction.Left)
-                    newPoints += 1;
-                else
-                    newPoints += 1001;
-                var leftList = new List<Direction>(moves);
-                var leftPositions = new HashSet<Point>(positions);
-                CheckPath(input, new Point(currentPosition.row, currentPosition.column - 1), length, width, Direction.Left, leftList, leftPositions, newPoints);
-            }
-            return false;
+            distances[startX, startY] = 0;
+            return distances;
         }
     }
 }
